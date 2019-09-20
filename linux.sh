7b95cb3bfb2b7f3e575a3c81c48fcad2ac7d775a
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-09-20 18:11:51 +0200

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,10 @@
+
+sudo apt-get install npm
+sudo apt-get install nodejs
+sudo npm install n
+sudo n stable
+sudo npm install discord.js
+sudo npm install express
+git pull https://github.com/tudbut/bottyclient.git
+echo All libraries should be installed now - Starting!
+node server.js
