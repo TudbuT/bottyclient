91230d7ff70cf6bb67e428179c0a757a77fc399b
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-09-20 19:02:26 +0200

    Update android.sh

--- android.sh
+++ android.sh
@@ -4,5 +4,5 @@ npm install n
 npm install discord.js
 npm install express
 git pull https://github.com/tudbut/bottyclient.git
-echo ××===== All libraries should be installed now - Starting! =====××
+echo ((((( All libraries should be installed now - Starting! )))))
 node server.js
