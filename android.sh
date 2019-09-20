85861c206f2fbaafc2170dcc0df1c54f28e7d375
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-09-20 18:18:00 +0200

    Update and rename android.sh to android-start.sh

--- android.sh
+++
@@ -1,7 +0,0 @@
-pkg install npm
-pkg install nodejs
-npm install discord.js
-npm install express
-git pull https://github.com/tudbut/bottyclient.git
-echo All libraries should be installed now - Starting!
-node server.js
