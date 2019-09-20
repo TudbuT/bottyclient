fe704a0fbe44bd7a975c794721a96a426c5c6739
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-09-20 18:18:59 +0200

    Delete android-start.sh

--- android-start.sh
+++
@@ -1,6 +0,0 @@
-echo You may need to run 'pkg install npm' and 'pkg install nodejs' if this is the first time you use this!
-npm install discord.js
-npm install express
-git pull https://github.com/tudbut/bottyclient.git
-echo Updated - Starting!
-node server.js
