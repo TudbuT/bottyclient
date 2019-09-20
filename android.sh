5d1f199dab467da1b9544291e60c543f6534301f
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-09-20 18:56:34 +0200

    Update android.sh

--- android.sh
+++ android.sh
@@ -1,7 +1,6 @@
 apt-get install npm
 apt-get install nodejs
 npm install n
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 npm install discord.js
 npm install express
 git pull https://github.com/tudbut/bottyclient.git
