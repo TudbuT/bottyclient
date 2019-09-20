fa1406ee73ecdb4af078fd8ed49b2d2c296346b2
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-09-20 18:13:43 +0200

    Create android.sh

---
+++ android.sh
@@ -0,0 +1,9 @@
+apt-get install npm
+apt-get install nodejs
+npm install n
+n stable
+npm install discord.js
+npm install express
+git pull https://github.com/tudbut/bottyclient.git
+echo All libraries should be installed now - Starting!
+node server.js
