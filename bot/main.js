e2c7fe20028f086c4e027f8d0b1b2d06d4769bca
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-09-27 20:11:19 +0200

    Update main.js

--- bot/main.js
+++ bot/main.js
@@ -47,7 +47,8 @@ module.exports = {
   },
   messages: function(channel) {
     var ht = new String("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">")
-    var msgs = gms(channel)
+    await gms(channel)
+    var msgs = remote
     console.log(msgs)
     ht = '<button type="button" onclick="window.location.href = `?logoff=1`">LogOff</button>' + ht + "<button type=\"button\" onclick=\"window.location.href = \`?path=send&channel=" + channel + "&msg=${x()}\`\">Send Message</button><script>var x = function () {return prompt('Message:').replace(\"#\", \"%23\").replace(\"&\", \"%26\")}</script><button type=\"button\" onclick=\"window.location.href = '?path=clist&channel=" + channel + "'\">Back to list</button>" + msgs
     
