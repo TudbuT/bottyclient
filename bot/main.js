b6289cbdc445f4818581a56de93c02bbfb21567f
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-09-27 18:49:20 +0100

    Update main.js

--- bot/main.js
+++ bot/main.js
@@ -46,7 +46,7 @@ module.exports = {
   },
   messages: function(channel) {
     var ht = new String("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">")
-    var msgs = new String(gms())
+    var msgs = new String(gms(channel))
     
     ht = '<button type="button" onclick="window.location.href = `?logoff=1`">LogOff</button>' + ht + "<button type=\"button\" onclick=\"window.location.href = \`?path=send&channel=" + channel + "&msg=${x()}\`\">Send Message</button><script>var x = function () {return prompt('Message:').replace(\"#\", \"%23\").replace(\"&\", \"%26\")}</script><button type=\"button\" onclick=\"window.location.href = '?path=clist&channel=" + channel + "'\">Back to list</button>" + msgs
     
@@ -95,7 +95,7 @@ client.on('ready', () => {
   console.log("Ready.")
 })
 
-var gms = function () {
+var gms = function (channel) {
       var x = new String("<br /><br /><br />Messages: <br /><br />")
       client.channels.find(c => c.id == channel).fetchMessages({max: 10}).forEach(m => {
         x = x + "<br />" + m.author.tag + " -- " + m.content.replace("\n", "<br />")
