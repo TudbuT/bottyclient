5372ce9c6eb9d4c023c1f1f65dbc996b8e7f64a1
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-09-28 11:20:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -37,17 +37,6 @@ app.get('/', async function(req, re) {
     await bot.senddm(r.dm, r.msg)
     re.send(await bot.dmmessages(r.dm) + style)
   }
-  if(r.path == "send" && r.channel) {
-    await bot.send(r.channel, r.msg)
-    re.send(await bot.messages(r.channel))
-  }
-  if(r.path == "dm" && r.dm) {
-    re.send(await bot.dmmessages(r.dm))
-  }
-  if(r.path == "senddm" && r.dm) {
-    await bot.senddm(r.dm, r.msg)
-    re.send(await bot.dmmessages(r.dm))
-  }
   if(r.path == "leave" && r.guild) {
     await bot.leave(r.guild)
     re.send(bot.selectGuild() + style)
@@ -91,46 +80,6 @@ app.get('/', async function(req, re) {
   }
   if(r.path == "clist" && r.channel) {
     re.send(bot.selectChannel(bot.getGuild(r.channel)) + style)
-  }
-  if(r.path == "delete" && r.channel && r.guild) {
-    await bot.deletech(r.channel)
-    re.send(bot.selectChannel(r.guild))
-  }
-  if(r.path == "deleter" && r.role && r.guild) {
-    await bot.deleter(r.role, r.guild)
-    re.send(bot.selectRole(r.guild))
-  }
-  if(r.path == "deletem" && r.member && r.guild) {
-    await bot.deletem(r.member, r.guild)
-    re.send(bot.selectMember(r.guild))
-  }
-  if(r.path == "ms" && r.guild) {
-    re.send(bot.selectMember(r.guild))
-  }
-  if(r.path == "delM" && r.channel && r.message) {
-    await bot.delM(r.message, r.channel)
-    re.send(await bot.messages(r.channel))
-  }
-  if(r.path == "delMdm" && r.dm && r.message) {
-    await bot.delMdm(r.message, r.dm)
-    re.send(await bot.dmmessages(r.dm))
-  }
-  if(r.path == "roles" && r.guild) {
-    re.send(bot.selectRole(r.guild))
-  }
-  if(r.path == "create" && r.name && r.type && r.guild) {
-    await bot.create(r.name, {type: r.type}, r.guild)
-    re.send(bot.selectChannel(r.guild))
-  }
-  if(r.path == "crro" && r.name && r.guild) {
-    await bot.createRole(r.name, r.perms, r.guild)
-    re.send(bot.selectRole(r.guild))
-  }
-  if(r.path == "list") {
-    re.send(bot.selectGuild())
-  }
-  if(r.path == "clist" && r.channel) {
-    re.send(bot.selectChannel(bot.getGuild(r.channel)))
   }
   if(r.logoff == "1") {
     re.sendFile(__dirname + "/viewer/index.html")
