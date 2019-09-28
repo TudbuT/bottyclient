25bc3ce29d67769e4a6289c3ef58ae6e2e851f1f
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-09-28 20:40:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const port = 4000
 
 
-const style = "<style>body {background-color: #2C2F33; color: #CCCCCC} button {background-color: #99AAB5; color: #FFF; height: 2em; border-radius: 8px; border: 1px solid #2C2F33; cursor: pointer;}</style>"
+const style = "<style>body {background-color: #2C2F33; color: #CCCCCC; font-family: Whitney, Arial} button {background-color: #99AAB5; color: #FFF; height: 2em; border-radius: 8px; border: 1px solid #2C2F33; cursor: pointer;}</style>"
 const b = ""
 
 const express = require('express');
