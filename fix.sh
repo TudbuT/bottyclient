0ec4f41bba0969553febe20f106005c171a12f28
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-09-29 18:47:29 +0200

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -2,4 +2,4 @@ cd ..
 rm -r -f bottyclient/
 git clone https://github.com/TudbuT/bottyclient
 cd bottyclient
-echo Now run your installer: Android - `bash android.sh` -- Linux - `bash linux.sh`
+echo Now run your installer (bash android.sh OR bash linux.sh)
