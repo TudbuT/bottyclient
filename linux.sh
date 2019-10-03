6536ab845ecfe907f21b16a8b6c11b66f6a1deaf
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-10-03 19:45:13 +0100

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -4,6 +4,7 @@ sudo apt-get install nodejs
 sudo npm install n
 sudo n stable
 sudo npm install -g npm
+sudo npm install node-opus
 sudo npm install discord.js
 sudo npm install express
 git pull https://github.com/tudbut/bottyclient.git
