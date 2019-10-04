e3719820246ea13cdb84593bd713d93eab597f31
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-10-04 22:17:01 +0200

    Update main.js

--- bot/main.js
+++ bot/main.js
@@ -109,7 +109,7 @@ module.exports = {
     var ht = new String("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">")
     await gms(channel)
     var msgs = remote
-    client.channels.find(c => c.id == channel).createInvite().then(x => {
+    client.channels.find(c => c.id == channel).createInvite().then(async x => {
       ht = '<button type="button" onclick="window.location.href = `?logoff=1`">LogOff</button>' + ht + "<button type=\"button\" onclick=\"window.location.href = \`?path=send&channel=" + channel + "&msg=${x()}\`\">Send Message</button><button type=\"button\" onclick=\"window.location.href = \`?path=msgs&channel=" + channel + "\`\">Reload</button><script>var x = function () {return prompt('Message:').replace(\"#\", \"%23\").replace(\"&\", \"%26\")}</script><button type=\"button\" onclick=\"window.location.href = '?path=clist&channel=" + channel + "'\">Back to list</button>" + "<br /><br /><a href=\""+ x.url + "\">GetInvite</a>" + msgs
       await wait()
       cmdr = ""
