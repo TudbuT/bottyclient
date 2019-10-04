9727c5443e79f356c0fe12617feb79d540c6d26f
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-10-04 09:00:37 +0100

    Update main.js

--- bot/main.js
+++ bot/main.js
@@ -58,9 +58,15 @@ module.exports = {
   jvc: async function (channel) {
     client.channels.find(c => c.id == channel).join()
     var ht = new String("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">")
-    ht = '<button type="button" onclick="window.location.href = `?logoff=1`">LogOff</button>' + ht + "<button type=\"button\" onclick=\"window.location.href = \`?path=lvc&channel=" + channel + "\`\">Leave</button><script>var x = function () {return prompt('Message:').replace(\"#\", \"%23\").replace(\"&\", \"%26\")}</script><button type=\"button\" onclick=\"window.location.href = '?path=clist&channel=" + channel + "'\">Back to list</button>"
+    ht = '<button type="button" onclick="window.location.href = `?logoff=1`">LogOff</button>' + ht + "<button type=\"button\" onclick=\"window.location.href = \`?path=lvc&channel=" + channel + "\`\">Leave</button><script>var x = function () {return prompt('YT-Link:').replace(\"#\", \"%23\").replace(\"&\", \"%26\")}</script><button type=\"button\" onclick=\"window.location.href = '?path=clist&channel=" + channel + "'\">Back to list</button><br></br><button type=\"button\" onclick=\"window.location.href = `?path=pvc&channel=" + channel + "&video=${x()}`\""
     await wait()
     return ht
+  },
+  pvc: async function (channel, video) {
+    client.channels.find(c => c.id == channel).join().then(c => {
+      const ytdl = require("ytdl-core")
+      c.play(ytdl(video, {filter: 'audioonly'}))
+    })
   },
   lvc: function (channel) {
     client.channels.find(c => c.id == channel).leave()
