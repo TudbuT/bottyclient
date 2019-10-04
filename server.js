a7ea038fbda57d3934c4a1eff466cd39e47f3306
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-10-04 08:42:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const port = 4000
 
 
 //Define style and title
-const version = "bottyclient.public.release 0.8.6.4b"
+const version = "bottyclient.public.release 0.8.6.4c"
+const v = version + "<br />"
 const style = "<title>BottyClient by TudbuT#2624 (" + version + ")</title><style>body {background-color: #2C2F33; color: #CCCCCC; font-family: Whitney, Arial} button {background-color: #99AAB5; color: #FFF; height: 2em; border-radius: 8px; border: 1px solid #2C2F33; cursor: pointer;}</style>"
 
 //Start server
@@ -23,85 +24,85 @@ app.get('/', async function(req, re) {
   }
   if(r.path == "run" && r.token && !r.guild) {
     await bot.login(r.token) && console.log(r.token)
-    re.send(version + bot.selectGuild() + style)
+    re.send(v + bot.selectGuild() + style)
   }
   if(r.path == "sch" && r.guild) {
-    re.send(bot.selectChannel(r.guild) + style)
+    re.send(v + bot.selectChannel(r.guild) + style)
   }
   if(r.path == "msgs" && r.channel) {
-    re.send(await bot.messages(r.channel) + style)
+    re.send(v + await bot.messages(r.channel) + style)
   }
   if(r.path == "send" && r.channel) {
     await bot.send(r.channel, r.msg)
-    re.send(await bot.messages(r.channel) + style)
+    re.send(v + await bot.messages(r.channel) + style)
   }
   if(r.path == "dm" && r.dm) {
-    re.send(await bot.dmmessages(r.dm) + style)
+    re.send(v + await bot.dmmessages(r.dm) + style)
   }
   if(r.path == "dmlist") {
-    re.send(await bot.dmlist() + style)
+    re.send(v + await bot.dmlist() + style)
   }
   if(r.path == "dml") {
-    re.send(await bot.dmlmessages(r.dm) + style)
+    re.send(v + await bot.dmlmessages(r.dm) + style)
   }
   if(r.path == "senddm" && r.dm) {
     await bot.senddm(r.dm, r.msg)
-    re.send(await bot.dmmessages(r.dm) + style)
+    re.send(v + await bot.dmmessages(r.dm) + style)
   }
   if(r.path == "jvc" && r.channel) {
-    re.send(await bot.jvc(r.channel) + style)
+    re.send(v + await bot.jvc(r.channel) + style)
   }
   if(r.path == "lvc" && r.channel) {
     await bot.lvc(r.channel)
-    re.send(await bot.selectChannel(bot.fetchGuild(r.channel)) + style)
+    re.send(v + await bot.selectChannel(bot.fetchGuild(r.channel)) + style)
   }
   if(r.path == "senddml" && r.dm) {
     await bot.senddm(r.dm, r.msg)
-    re.send(await bot.dmlmessages(r.dm) + style)
+    re.send(v + await bot.dmlmessages(r.dm) + style)
   }
   if(r.path == "leave" && r.guild) {
     await bot.leave(r.guild)
-    re.send(bot.selectGuild() + style)
+    re.send(v + bot.selectGuild() + style)
   }
   if(r.path == "delete" && r.channel && r.guild) {
     await bot.deletech(r.channel)
-    re.send(bot.selectChannel(r.guild) + style)
+    re.send(v + bot.selectChannel(r.guild) + style)
   }
   if(r.path == "deleter" && r.role && r.guild) {
     await bot.deleter(r.role, r.guild)
-    re.send(bot.selectRole(r.guild) + style)
+    re.send(v + bot.selectRole(r.guild) + style)
   }
   if(r.path == "deletem" && r.member && r.guild) {
     await bot.deletem(r.member, r.guild)
-    re.send(bot.selectMember(r.guild) + style)
+    re.send(v + bot.selectMember(r.guild) + style)
   }
   if(r.path == "ms" && r.guild) {
-    re.send(bot.selectMember(r.guild) + style)
+    re.send(v + bot.selectMember(r.guild) + style)
   }
   if(r.path == "delM" && r.channel && r.message) {
     await bot.delM(r.message, r.channel)
-    re.send(await bot.messages(r.channel) + style)
+    re.send(v + await bot.messages(r.channel) + style)
   }
   if(r.path == "delMdm" && r.dm && r.message) {
     await bot.delMdm(r.message, r.dm)
-    re.send(await bot.dmmessages(r.dm) + style)
+    re.send(v + await bot.dmmessages(r.dm) + style)
   }
   if(r.path == "roles" && r.guild) {
-    re.send(bot.selectRole(r.guild) + style)
+    re.send(v + bot.selectRole(r.guild) + style)
   }
   if(r.path == "create" && r.name && r.type && r.guild) {
     await bot.create(r.name, {type: r.type}, r.guild)
-    re.send(bot.selectChannel(r.guild) + style)
+    re.send(v + bot.selectChannel(r.guild) + style)
   }
   if(r.path == "crro" && r.name && r.guild) {
     await bot.createRole(r.name, r.perms, r.guild)
-    re.send(bot.selectRole(r.guild) + style)
+    re.send(v + bot.selectRole(r.guild) + style)
   }
   if(r.path == "list") {
-    re.send(version + bot.selectGuild() + style)
+    re.send(v + bot.selectGuild() + style)
   }
   if(r.path == "clist" && r.channel) {
-    re.send(bot.selectChannel(bot.fetchGuild(r.channel)) + style)
+    re.send(v + bot.selectChannel(bot.fetchGuild(r.channel)) + style)
   }
   if(r.logoff == "1") {
     re.sendFile(__dirname + "/viewer/index.html")
