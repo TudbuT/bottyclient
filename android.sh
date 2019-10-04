7797638ea2e00cec25a201c347e78fad4903f2ac
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-10-04 09:07:11 +0100

    Update android.sh

--- android.sh
+++ android.sh
@@ -5,6 +5,7 @@ npm install n
 npm install discord.js
 npm install express
 npm install node-opus
+npm install ytdl-core
 git pull https://github.com/tudbut/bottyclient.git
 echo ℹ️ All libraries should be installed now - Starting! ℹ️
 node server.js
