0a70184dd1a2d0bb6735a88bc2dcc4c09f532646
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-10-04 09:27:39 +0100

    Update android.sh

--- android.sh
+++ android.sh
@@ -4,7 +4,7 @@ apt-get install ffmpeg
 npm install n
 npm install discord.js
 npm install express
-npm install node-opus
+npm install opusscript
 npm install ytdl-core
 git pull https://github.com/tudbut/bottyclient.git
 echo ℹ️ All libraries should be installed now - Starting! ℹ️
