17a4a3f50c07c87caf8b71a90889387daf62b32f
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-10-10 11:13:27 +0100

    Update main.js

--- bot/main.js
+++ bot/main.js
@@ -114,7 +114,7 @@ module.exports = {
     return '<button type="button" onclick="window.location.href = `?logoff=1`">LogOff</button>' + selm
   },
   messages: async function(channel) {
-    remote = "ERR 2 (NO MESSAGES FOUND)"
+    remote = "<br />ERR 2 (NO MESSAGES FOUND)"
     var ht = new String("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">")
     await gms(channel)
     var msgs = remote
