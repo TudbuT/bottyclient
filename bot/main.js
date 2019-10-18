7599712112b4e7ca40751a458c6f5be72dc6ef6b
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-10-18 21:08:22 +0200

    Update main.js

--- bot/main.js
+++ bot/main.js
@@ -266,6 +266,7 @@ ${m.embeds[0].description}
 
 function wait () {} // just for timings
 function maut(m) {
-  if(m.author) if(m.author.name) return "[A]" + m.author.name + "\n"
-  else return "";
+  if(m.author) if(m.author.name) {
+    return "[A]" + m.author.name + "\n";
+  } else return "";
 }
