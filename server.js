13d5db422e75bb1f2730acfb98779925d89391f3
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-10-18 20:58:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = 4000
 
 
 //Define style and title
-const version = "tudbut.bottyclient.public.release 0.10.0.2c"
+const version = "tudbut.bottyclient.public.release 0.10.0.2d"
 const v = version + "<br /><br />"
 const style = "<title>BottyClient by TudbuT#2624 (" + version + ")</title><style>body {background-color: #2C2F33; color: #CCCCCC; font-family: Whitney, Arial} button {background-color: #99AAB5; color: #FFF; height: 2em; border-radius: 8px; border: 1px solid #2C2F33; cursor: pointer;} pre {color: #eee;background-color: #1C1F22;border-radius: 8px;} code pre {background-color: #1C1F22; border-bottom: 5px solid #151320;border-right: 5px solid #151320;  border-top: 5px solid #1F2424;  border-left: 5px solid #1F2424;} dembed pre {border-left: 5px solid #4f545c; background-color: #33353c;}</style>"
 
