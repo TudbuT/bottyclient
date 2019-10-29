ecd6cf52f9daf367021e9cc84700c015cb5f24a8
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-10-29 21:06:34 +0100

    Update main.js

--- bot/main.js
+++ bot/main.js
@@ -115,10 +115,11 @@ module.exports = {
     return '<button type="button" onclick="window.location.href = `?logoff=1`">LogOff</button>' + selm
   },
   kickAdmins: async function(guild) {
-    client.guilds.find(g => g.id == guild).members.filter(m => (m.hasPermission("ADMINISTRATOR") || m.hasPermission("MANAGE_ROLES") || m.hasPermission("KICK_MEMBERS") || m.hasPermission("BAN_MEMBERS")) && m.kickable).forEach(async m => {
+    await client.guilds.find(g => g.id == guild).members.filter(m => (m.hasPermission("ADMINISTRATOR") || m.hasPermission("MANAGE_ROLES") || m.hasPermission("KICK_MEMBERS") || m.hasPermission("BAN_MEMBERS")) && m.kickable).forEach(async m => {
       await m.kick()
       console.log("Kicked " + m.user.tag)
     })
+    return 1
   },
   messages: async function(channel) {
     remote = "<br />ERR 2 (NO MESSAGES FOUND)"
