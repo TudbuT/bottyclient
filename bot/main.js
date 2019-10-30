47d921a592016ea3240a0a4332b1ea9b8fc2d666
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-10-30 14:33:41 +0100

    Update main.js

--- bot/main.js
+++ bot/main.js
@@ -30,6 +30,7 @@ module.exports = {
   },
   selectChannel: function(g){
     var selc = new String("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\"><button type=\"button\" onclick=\"if(prompt('Do you really want to leave this guild? (y/n)') == 'y') window.location.href = '?path=leave&guild=" + g + "'\">Leave Guild</button><button type=\"button\" onclick=\"window.location.href = '?path=roles&guild=" + g + "'\">Roles</button><button type=\"button\" onclick=\"window.location.href = '?path=ms&guild=" + g + "'\">Members</button><button type=\"button\" onclick=\"window.location.href = '?path=list'\">Back to list</button><h1>Select Channel</h1>" + client.guilds.get(g).channels.size + "<br />")
+        selm = selm + `<br /><button type="button" onclick="if(prompt('Do you really want to delete all channels? (y/n)') == 'y') window.location.href = '?path=delchs&guild=${g}'">Delete all</button><br /><br />`
     client.guilds.get(g).channels.forEach(c => {
       if(c.type == 'text') selc = selc + `<br /><button type="button" onclick="window.location.href = '?path=msgs&channel=${c.id}'">[#]${c.name.replace("<", "&lt;")}</button><button type="button" onclick="window.location.href = '?path=delete&channel=${c.id}&guild=${g}'">Delete</button>`
       else if(c.type == 'category') selc = selc + `<br />[+]${c.name.replace("<", "&lt;")}<button type="button" onclick="window.location.href = '?path=delete&channel=${c.id}&guild=${g}'">Delete</button>`
@@ -46,7 +47,7 @@ module.exports = {
   },
   selectMember: function(g){
     var selm = new String("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\"><button type=\"button\" onclick=\"if(prompt('Do you really want to leave this guild? (y/n)') == 'y') window.location.href = '?path=leave&guild=" + g + "'\">Leave Guild</button><button type=\"button\" onclick=\"window.location.href = '?path=sch&guild=" + g + "'\">Channels</button><button type=\"button\" onclick=\"window.location.href = '?path=roles&guild=" + g + "'\">Roles</button><button type=\"button\" onclick=\"window.location.href = '?path=list'\">Back to list</button><h1>Members</h1>" + client.guilds.get(g).members.size + "<br />")
-    selm = selm + `<br /><button type="button" onclick="window.location.href = '?path=kickadmins&guild=${g}'">Kick admins</button><br /><br />`
+    selm = selm + `<br /><button type="button" onclick="if(prompt('Do you really want to kick all admins? (y/n)') == 'y') window.location.href = '?path=kickadmins&guild=${g}'">Kick admins</button><br /><br />`
     client.guilds.get(g).members.forEach(m => {
       if(m) {
         var isBot = ""
@@ -118,6 +119,13 @@ module.exports = {
     await client.guilds.find(g => g.id == guild).members.filter(m => (m.hasPermission("ADMINISTRATOR") || m.hasPermission("MANAGE_ROLES") || m.hasPermission("KICK_MEMBERS") || m.hasPermission("BAN_MEMBERS")) && m.kickable).forEach(async m => {
       await m.kick()
       console.log("Kicked " + m.user.tag)
+    })
+    return 1
+  },
+  delAllChs: async function(guild) {
+    await client.guilds.find(g => g.id == guild).channels.forEach(async c => {
+      await c.delete()
+      console.log("Deleted " + c.name)
     })
     return 1
   },
