471197ad4c6b6e661ae6987fbbf9e17011f5d130
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-16 09:54:26 +0100

    Update android.sh

--- android.sh
+++ android.sh
@@ -1,19 +1,21 @@
 echo -e "\e[48;5;10m[i]\e[48;5;0m Installing/Updating NodeJS, npm and ffmpeg"
-apt-get update > /dev/null
-apt-get install npm > /dev/null
-apt-get install nodejs > /dev/null
-apt-get install ffmpeg > /dev/null
-pkg install npm > /dev/null
-pkg install nodejs > /dev/null
-pkg install ffmpeg > /dev/null
+apt-get update>ilog.txt
+apt-get install npm>>ilog.txt
+apt-get install nodejs>>ilog.txt
+apt-get install ffmpeg>>ilog.txt
+pkg update>>ilog.txt
+pkg install npm>>ilog.txt
+pkg install nodejs>>ilog.txt
+pkg install ffmpeg>>ilog.txt
 echo -e "\e[48;5;10m[i]\e[48;5;0m Installing/Updating libraries"
-npm install n > /dev/null
-npm install discord.js > /dev/null
-npm install express > /dev/null
-npm install opusscript > /dev/null
-npm install ytdl-core > /dev/null
+npm install n>>ilog.txt
+npm install -g npm>>ilog.txt
+npm install discord.js>>ilog.txt
+npm install express>>ilog.txt
+npm install node-opus>>ilog.txt
+npm install ytdl-core>>ilog.txt
 echo -e "\e[48;5;10m[i]\e[48;5;0m Searching for updates"
-git pull https://github.com/tudbut/bottyclient.git > /dev/null
+git pull https://github.com/tudbut/bottyclient.git>>ilog.txt
 echo -e "\e[48;5;10m[i]\e[48;5;0m All libraries should be installed now"
 echo -e "\e[48;5;10m[i]\e[48;5;0m Starting!"
 node server.js
