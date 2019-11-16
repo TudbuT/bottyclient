40fb9ecc9195c8177b470f60e07e7502e07ef115
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-16 14:47:28 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 echo off
 cls
 echo [i] Updating BC
-git pull https://github.com/TudbuT/bottyclient.git windows
+git pull https://github.com/TudbuT/bottyclient.git windows >> log.txt
 echo [i] Starting
 node server.js
