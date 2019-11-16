85edb82e6dcdf6e5e7f4589f2b04c8b633e848df
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-16 14:29:13 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 BASH=/bin/bash
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Updating BC"
-git pull -s recursive https://github.com/TudbuT/bottyclient.git &> rlog.txt
+git pull -s recursive https://github.com/TudbuT/bottyclient.git > rlog.txt 2>&1
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Starting"
 node server.js
