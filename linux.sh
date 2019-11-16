96ee07db2b78b8946b358e70a2d5a71e5a49205d
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-16 09:53:01 +0100

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,17 +1,17 @@
 echo -e "\e[48;5;10m[i]\e[48;5;0m Installing/Updating NodeJS, npm and ffmpeg"
-apt-get update > /dev/null
-apt-get install npm > /dev/null
-apt-get install nodejs > /dev/null
-apt-get install ffmpeg > /dev/null
+apt-get update>ilog.txt
+apt-get install npm>>ilog.txt
+apt-get install nodejs>>ilog.txt
+apt-get install ffmpeg>>ilog.txt
 echo -e "\e[48;5;10m[i]\e[48;5;0m Installing/Updating libraries"
-npm install n > /dev/null
-npm install -g npm > /dev/null
-npm install discord.js > /dev/null
-npm install express > /dev/null
-npm install node-opus > /dev/null
-npm install ytdl-core > /dev/null
+npm install n>>ilog.txt
+npm install -g npm>>ilog.txt
+npm install discord.js>>ilog.txt
+npm install express>>ilog.txt
+npm install node-opus>>ilog.txt
+npm install ytdl-core>>ilog.txt
 echo -e "\e[48;5;10m[i]\e[48;5;0m Searching for updates"
-git pull https://github.com/tudbut/bottyclient.git > /dev/null
+git pull https://github.com/tudbut/bottyclient.git>>ilog.txt
 echo -e "\e[48;5;10m[i]\e[48;5;0m All libraries should be installed now"
 echo -e "\e[48;5;10m[i]\e[48;5;0m Starting!"
 node server.js
