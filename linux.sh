f13a34e50746738201ce96cbe3e7e7d108e72f2e
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-21 22:21:29 +0100

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -15,7 +15,7 @@ git config user.email "x@y.z"
 git config user.name "XYZ"
 git pull https://github.com/tudbut/bottyclient.git>>ilog.txt 2>&1
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m All libraries should be installed now"
-echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Creating terminal command 'bottyclient' (this requires administrator)"
+echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Creating terminal command 'bottyclient' (this requires administrator) and 'nbottyclient' (this does not require administrator)"
 sudo echo `cd ${PWD}`>/bin/bottyclient
 sudo echo `bash run.sh`>>/bin/bottyclient
 sudo chmod +x /bin/bottyclient
