a87d9b11a07089f996be129a427cdf0367a173bc
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-22 21:44:33 +0100

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -15,13 +15,9 @@ git config user.email "x@y.z"
 git config user.name "XYZ"
 git pull https://github.com/tudbut/bottyclient.git>>ilog.txt 2>&1
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m All libraries should be installed now"
-echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Creating terminal command 'bottyclient' (this requires administrator) and 'nbottyclient' (this does not require administrator)"
+echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Creating terminal command 'bottyclient' (this requires administrator)"
 sudo echo `cd ${PWD}`>/bin/bottyclient
 sudo echo `bash run.sh`>>/bin/bottyclient
 sudo chmod +x /bin/bottyclient
-echo `cd ${PWD}`>~/nbottyclient
-echo `bash run.sh`>~/nbottyclient
-chmod +x ~/nbottyclient
-echo `alias nbottyclient="~/nbottyclient"`>>~/.bash_profile
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Starting!"
 node server.js
