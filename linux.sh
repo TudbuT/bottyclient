ae7d752092ceb1ef109c257bbf84d9beb64835df
Daniel H <dh.danielhister@gmail.com>
2020-03-23 18:07:24 +0100

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,7 +1,7 @@
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Installing/Updating NodeJS, npm and ffmpeg"
 yes Y | apt-get update>ilog.txt 2>&1
 yes Y | apt-get install npm>>ilog.txt 2>&1
-yes Y | apt-get install nodejs>>ilog.txt 2>&1
+yes Y | apt-get install nodejs-legacy>>ilog.txt 2>&1
 yes Y | apt-get install ffmpeg>>ilog.txt 2>&1
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Installing/Updating libraries"
 npm i>>ilog.txt 2>&1
