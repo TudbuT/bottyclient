a981fe8b9a735e4aba5bc81c762059cd621f98b2
Daniel H <dh.danielhister@gmail.com>
2020-09-26 17:40:49 +0200

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -5,6 +5,7 @@ yes Y | apt-get install nodejs-legacy>>ilog.txt 2>&1
 yes Y | apt-get install ffmpeg>>ilog.txt 2>&1
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Installing/Updating libraries"
 npm i>>ilog.txt 2>&1
+n latest>>ilog.txt 2>&1
 npm install -g npm>>ilog.txt 2>&1
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Searching for updates"
 git config user.email "x@y.z"
