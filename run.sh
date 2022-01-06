1dc8b9bc85fe7364ce239e63756eb41d0571252f
TudbuT <tudbut@tudbut.de>
2022-01-06 04:09:20 +0100

    windows install & run files (report any issues you encounter please)

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-BASH=/bin/bash
+#!/bin/bash
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Updating BC"
 git pull -s recursive https://github.com/TudbuT/bottyclient.git > rlog.txt 2>&1
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Starting"
---
+++ windows.bat
@@ -0,0 +1,3 @@
+curl.exe -o node.msi https://nodejs.org/dist/v16.13.1/node-v16.13.1-x86.msi
+msiexec /i node.msi
+npm install
